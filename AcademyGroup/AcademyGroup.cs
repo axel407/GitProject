ce8272c9cea833a93bfd82d1730f4bc1a980ba25
axel407 <Axel_Jkit@protonmail.com>
2023-08-14 15:09:58 +0300

    #Task6 Добавление библеотеки классов AcademyGroup и класса AcademyGroup
    Добавление библеотеки классов и класса AcademyGroup необходимого для создание списков студентов

---
+++ AcademyGroup/AcademyGroup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace AcademyGroup
+{
+    [Serializable]
+    [KnownType(typeof(Person.Person))]
+    [KnownType(typeof(Student.Student))]
+    [XmlInclude(typeof(Person.Person))]
+    [XmlInclude(typeof(Student.Student))]
+    [DataContract]
+    public class AcademyGroup
+    {
+        [DataMember]
+        private ArrayList group = new ArrayList();
+
+        public AcademyGroup()
+        {
+            group.Clear();
+        }
+    }
+}
