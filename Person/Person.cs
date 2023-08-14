22333d8656c33ae300e44b69ee31d96b0a301a29
axel407 <Axel_Jkit@protonmail.com>
2023-08-14 14:35:20 +0300

    #Task1 Добавление библиотеки класса Person
    Добавление библиотеки класса Person с классом Person. Класс предназначен для описание сущности Person

---
+++ Person/Person.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.Serialization;
+
+namespace Person
+{
+    [Serializable]
+    [DataContract]
+    public class Person
+    {
+        //поля класса Person
+        private string name = "Un";
+        private string surname = "Un";
+        private int age;
+        private string phone = "Un";
+
+        //свойства класса Person
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                try
+                {
+                    name = value;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+        [DataMember]
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                try
+                {
+                    surname = value;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+        [DataMember]
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                try
+                {
+                    if (value > 0)
+                        age = value;
+                    else
+                        throw new Exception("Wrong age value! Try again");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+        [DataMember]
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                try
+                {
+                    phone = value;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
